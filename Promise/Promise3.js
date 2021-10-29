36df0fc89868aec31000df63bf75dd03a50bddec
JJLee-20194099 <JJLee-20194099>
2021-10-29 11:58:47 +0700

    Promise.resolve Promise.reject Promise.all

--- Promise/Promise3.js
+++ Promise/Promise3.js
@@ -9,46 +9,55 @@
 // 2. Fullfilled: Trạng thái thành công
 // 3. Rejected: Thất bại
 
+// 1. Promise.resolve
+// 2. Promise.reject
+// 3. Promise.all
+
 // -> XỬ LÝ CÁO THAO TÁC BẤT DỒNG BỘ
 // Phiên bản js es6
 
-var cnt = 0
+// var cnt = 0
 
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms)
-    })
-} 
+// function sleep(ms) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, ms)
+//     })
+// } 
 
-sleep(1000)
-    .then(function() {
-        console.log(1)
-        return sleep(1000)
-    })
-    .then(function() {
-        console.log(2)
-        return sleep(1000)
-    })
-    .then(function() {
-        console.log(3)
-        return sleep(1000)
-    })
+// sleep(1000)
+//     .then(function() {
+//         console.log(1)
+//         return sleep(1000)
+//     })
+//     .then(function() {
+//         console.log(2)
+//         return new Promise(function(resolve, reject) {
+//             reject('failure')
+//         })
+//     })
+//     .then(function() {
+//         console.log(3)
+//         return sleep(1000)
+//     })
+//     .catch(function(error) {
+//         console.log(error)
+//     })
 
-var promise = new Promise(
-    // Executor
-    // Khi gọi new Promise thì Executor function được thực thi trước khi
-    // gán cho promise luôn anh em nhé
-    function(resolve, reject) {
-        // Logic
-        // Thành công resolve()
-        // Thất bạt reject()
+// var promise = new Promise(
+//     // Executor
+//     // Khi gọi new Promise thì Executor function được thực thi trước khi
+//     // gán cho promise luôn anh em nhé
+//     function(resolve, reject) {
+//         // Logic
+//         // Thành công resolve()
+//         // Thất bạt reject()
         
-        // Fake call API
-        // reject('Có lỗi!!!')
+//         // Fake call API
+//         // reject('Có lỗi!!!')
 
-        resolve()
-    }
-);
+//         resolve()
+//     }
+// );
 
 // How i use Promise
 
@@ -78,4 +87,47 @@ var promise = new Promise(
 //     })
 
 
+// var successfulPromise = Promise.resolve('Sucsess!!!')
+
+// successfulPromise
+//     .then(function(result) {
+//         console.log('result: ', result)
+//     })
+
+/*
+    -> Vấn đề : Mất 7s để chạy 2 hàm bất đồng bộ không phụ thuộc vào nhau
+    var promise1 = new promises(function(resolve) {
+        setTimeout(function() {
+            resolve([1]);
+        }, 2000)
+    })
+
+    var promise2 = new promises(function(resolve) {
+        setTimeout(function() {
+            resolve([2, 3]);
+        }, 5000)
+    })
+
+*/
+
+// -> Giải quyết
+
+var promise1 = new Promise(function(resolve) {
+    setTimeout(function() {
+        resolve([1]);
+    }, 2000)
+})
+
+var promise2 = new Promise(function(resolve) {
+    setTimeout(function() {
+        resolve([2, 3]);
+    }, 3000)
+})
+
+Promise.all([promise1, promise2])
+    .then(function(result) {
+        const res1 = result[0]
+        const res2 = result[1]
+        console.log(res1.concat(res2))
+    })
 
